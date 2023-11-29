a3f77cc67abee133f0c55b2417d41397cd3585cb
MJAguda <mark.jayson.aguda@gmail.com>
2023-11-30 00:18:16 +0800

    Added greeting.jsp

--- src/main/java/com/pluralsight/conference/controller/GreetingController.java
+++ src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class GreetingController {
     @GetMapping("greeting")
     public String greeting(Map<String, Object> model){
-        model.put("message", "Hello Bryan");
+        model.put("message", "Hello Jayson");
         return "greeting";
     }
 }
