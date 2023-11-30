4f15aa5e6283f211fd7339e3c1ab12075e06a6a2
MJAguda <mark.jayson.aguda@gmail.com>
2023-11-30 23:13:01 +0800

    Creating Controllers in Spring MVC Applications

--- src/main/java/com/pluralsight/conference/ConferenceApplication.java
+++ src/main/java/com/pluralsight/conference/ConferenceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConferenceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConferenceApplication.class, args);
+		SpringApplication. run(ConferenceApplication.class, args);
 	}
 
 }
