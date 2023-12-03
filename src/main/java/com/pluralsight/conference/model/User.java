8f53e59ee56cae188573a172973e8888cb7ef102
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-03 18:32:03 +0800

    Add User model class

---
+++ src/main/java/com/pluralsight/conference/model/User.java
@@ -0,0 +1,24 @@
+package com.pluralsight.conference.model;
+
+public class User {
+    private long id;
+    private String Name;
+
+    public long getId() {
+        return id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return Name;
+    }
+    
+    public void setName(String name) {
+        Name = name;
+    }
+
+    
+}
