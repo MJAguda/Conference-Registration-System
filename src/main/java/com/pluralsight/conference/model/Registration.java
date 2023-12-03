47575b1005aa9190af3c03eb6b2b1521693c99e4
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-03 15:59:29 +0800

    Added @NotEmpty annotation

--- src/main/java/com/pluralsight/conference/model/Registration.java
+++ src/main/java/com/pluralsight/conference/model/Registration.java
@@ -1,6 +1,12 @@
 package com.pluralsight.conference.model;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Registration {
+    
+    // @NotEmpty is a validation annotation that checks if the field is not empty 
+    // making sure that the user enters a value for the name field
+    @NotEmpty
     private String name;
 
     public String getName() {
