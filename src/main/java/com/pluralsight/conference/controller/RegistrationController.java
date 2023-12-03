423084d2b88bcaa464acf857c47d2522da22af0c
MJAguda <mark.jayson.aguda@gmail.com>
2023-12-03 16:00:25 +0800

    Added @Valid annotation and Binded an error messag

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -1,11 +1,14 @@
 package com.pluralsight.conference.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.pluralsight.conference.model.Registration;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -19,10 +22,18 @@ public class RegistrationController {
 
     // HTTP POST requests to the "/registration" endpoint if the Add Registration button is clicked, wherein it processes form data, prints the name from the Registration object to the console
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute ("registration") Registration registration){
+    public String addRegistration(  @Valid @ModelAttribute ("registration") Registration registration, 
+                                    BindingResult result){
+        
+        // If there are errors, it will return the registration view
+        if(result.hasErrors()){
+            System.out.println("There were errors");
+            return "registration";
+        }
 
+        // If there are no errors, it will print the name from the 
+        //Registration object to the console and return the registration view
         System.out.println("Registration: " + registration.getName());
-
         return "registration";
     }
 }
